027f18833ff5336e271b2e47a5d073c513b7a51c
Luis Felipe Hernandez <felipe@numberai.com>
2022-03-13 13:47:13 -0400

    Remove support for up and down (w, s) keys

--- src/ecs/components/KeyboardController.hpp
+++ src/ecs/components/KeyboardController.hpp
@@ -15,15 +15,9 @@ class KeyboardController : public Component {
   void update() override {
     if (Game::event.type == SDL_KEYDOWN) {
       switch (Game::event.key.keysym.sym) {
-        // case SDLK_w:
-        //   transform->velocity.y = -1;
-        //   break;
         case SDLK_a:
           transform->velocity.x = -1;
           break;
-        // case SDLK_s:
-        //   transform->velocity.y = 1;
-        //   break;
         case SDLK_d:
           transform->velocity.x = 1;
           break;
@@ -36,15 +30,9 @@ class KeyboardController : public Component {
     }
     if (Game::event.type == SDL_KEYUP) {
       switch (Game::event.key.keysym.sym) {
-        // case SDLK_w:
-        //   transform->velocity.y = 0;
-        //   break;
         case SDLK_a:
           transform->velocity.x = 0;
           break;
-        // case SDLK_s:
-        //   transform->velocity.y = 0;
-        //   break;
         case SDLK_d:
           transform->velocity.x = 0;
           break;
